8d93cf1535c5c0843c004af6e2daf2ef96005a86
gianella <g.cevallos@gianellin.com>
2023-04-19 15:53:18 -0400

    add functionality to form

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+const form = document.querySelector('.right form');
+const inputs = document.querySelectorAll('.right form input');
 
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    inputs.forEach(input=> {
+        console.log(input.value);
+    })
+});
