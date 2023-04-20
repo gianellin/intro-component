d17f937ffd4480138a3f24a943838c93b3f1f422
gianella <g.cevallos@gianellin.com>
2023-04-19 21:58:42 -0400

    solved script error & added active state

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-const form = document.querySelector('.right form');
-const inputs = document.querySelectorAll('.right form input');
+const form = document.querySelector('.form-container form');
+const inputs = document.querySelectorAll('.form-container input');
 
-form.addEventListener('submit', (e) =>{
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    inputs.forEach(input=> {
-        console.log(input.value);
-    })
+    inputs.forEach((input) =>{
+        // console.log(input.value);
+        if (!input.value) {
+			input.parentElement.classList.add('error');
+		} else {
+			input.parentElement.classList.remove('error');
+			if (input.type == 'email') {
+				if (validateEmail(input.value)) {
+					input.parentElement.classList.remove('error');
+				} else {
+					input.parentElement.classList.add('error');
+				}
+			}
+		}
+    });
 });
+
